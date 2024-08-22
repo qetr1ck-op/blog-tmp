d57bc16ee067e83234701eac4eac95e69f601125
qetr1ck <orestprustayko@gmail.com>
2024-08-22 23:51:02 +0300

    Start to update personal information and summary in resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,43 +20,43 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Orest Prystaiko",
+  initials: "OP",
+  location: "Lviv, Ukraine, UTC+2",
+  locationLink: "https://www.google.com/maps/place/Lviv",
   about:
-    "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
+    "Detail-oriented Full Stack JS Engineer dedicated to architect|manage|building high-quality products.",
   summary:
-    "As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Full Stack JS Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js. I have over 12 years of experience working remotely with companies worldwide.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/5235395?v=4",
+  personalWebsiteUrl: "",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "orestprystayko@gmail.com",
+    tel: "+380685051465",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/qetr1ck-op",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/orestprystayko",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/oprystaiko",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Lviv Polytechnic National University",
+      degree: "Master's degree",
+      start: "2010",
+      end: "2005",
     },
   ],
   work: [
